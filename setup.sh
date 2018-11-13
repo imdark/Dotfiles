54ec3e99656bed9ac0f57b97c32ae5982b97e90f
imdark <mikesgoacademy@gmail.com>
2018-11-12 16:55:37 -0800

    added more stuff

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt-get install vim emacs tmux cstags scope htop curl wget
+sudo apt-get install vim emacs tmux cstags scope htop curl wget albert
 # node docker cbuildtools gstreamer ros
